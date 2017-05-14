d3262a1698366e080eb364ecbaf2ec44e49955b1
jparevalo <jparevalo@miuandes.cl>
2017-05-14 14:13:06 -0400

    Implemented all devise requirements, fixed navbar issue with new course view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email_confirmation])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :people
+  devise_for :people do
+    get "/people/sign_out" => "devise/sessions#destroy", :as => :destroy_person_session
+  end
   authenticate :person do
       resources :courses do
         resources :enrollments
@@ -9,8 +11,8 @@ Rails.application.routes.draw do
         get 'students', to: 'courses#students', on: :member
       end
       resources :people
-
       root to: 'courses#index'
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
